02089329eb2adda341e9605e62de091f22ec518a
sushmita <gupta.sushmita31@gmail.com>
2021-05-27 22:50:35 +0530

    Some warm up :)

---
+++ Most-asked-ques-EASY/src/StockBuyAndSell.java
@@ -0,0 +1,53 @@
+/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0
+
+Example : prices = [7,1,5,3,6,4]
+output : 5
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class StockBuyAndSellSolution
+{
+	public static int profit(int []prices)
+	{
+		int min = Integer.MAX_VALUE;
+		int profit = 0;
+		for(int i=0; i<prices.length; i++)
+		{
+			if(prices[i] < min)
+			{
+				min = prices[i];
+			}
+			else
+			{
+				profit = Math.max(profit, prices[i]-min);
+			}
+		}
+		return profit;
+	}
+}
+public class StockBuyAndSell {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the value of days : ");
+		int n = Integer.parseInt(br.readLine());
+		int prices[] = new int[n];
+		System.out.println("Enter the values of prices : ");
+		String line = br.readLine();
+		String strs[] = line.trim().split("\\s+");
+		for(int i=0; i<n; i++)
+		{
+			prices[i] = Integer.parseInt(strs[i]);
+		}
+		int maxProfit = StockBuyAndSellSolution.profit(prices);
+		System.out.println("The maxProfit is " + maxProfit);
+		
+	}
+
+}
---
+++ Most-asked-ques-EASY/src/TwoSum.java
@@ -0,0 +1,59 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order
+
+Example : nums[2,7,11,15] target = 9
+output  : [0,1]
+
+*/
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+
+class TwoSumSolution
+{
+	public static int[] twoSum(int nums[], int target)
+	{
+		HashMap<Integer,Integer> map = new HashMap<>();
+		for(int i=0; i<nums.length; i++)
+		{
+			map.put(nums[i], i);
+		}
+		for(int i=0; i<nums.length; i++)
+		{
+			int val = target - nums[i];
+			if(map.containsKey(val) && map.get(val)!=i)
+			{
+				return new int[] {i,map.get(val)};
+			}
+		}
+		return new int[] {};
+	}
+}
+public class TwoSum {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the value of n : ");
+		int n = Integer.parseInt(br.readLine());
+		int nums[] = new int[n];
+		System.out.println("Enter the values of nums : ");
+		String line = br.readLine();
+		String strs[] = line.trim().split("\\s+");
+		for(int i=0; i<n; i++)
+		{
+			nums[i] = Integer.parseInt(strs[i]);
+		}
+		System.out.println("Enter the target value : ");
+		int target = Integer.parseInt(br.readLine());
+		int sol[] = TwoSumSolution.twoSum(nums, target);
+		String ansSol = Arrays.toString(sol);
+		System.out.println("The output is : " + ansSol);
+	}
+
+}
